df2509e8a9072407ffc6f885c80ecf0e38b28008
MA <ari@nimaar.de>
2024-11-26 21:15:07 +0100

    ESP-32-KIT-CLOCK-NTP

---
+++ ESP-32-KIT-CLOCK-NTP/src/credentials.h
@@ -0,0 +1,8 @@
+#
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char chSSID[] = "SSID";               // WiFi SSID (replace with your own)
+const char chPassword[] = "PASSWORD";       // WiFi password (replace with your own)
+
+#endif
