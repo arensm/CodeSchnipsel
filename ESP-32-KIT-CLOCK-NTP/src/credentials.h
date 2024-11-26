db45f5e2b6f6f0b7783ebaf6c2d69a943f9eac5a
MA <ari@nimaar.de>
2024-11-26 21:01:35 +0100

    Clean up ignored files

--- ESP-32-KIT-CLOCK-NTP/src/credentials.h
+++
@@ -1,8 +0,0 @@
-#
-#ifndef CREDENTIALS_H
-#define CREDENTIALS_H
-
-const char chSSID[] = "SSID";               // WiFi SSID (replace with your own)
-const char chPassword[] = "PASSWORD";       // WiFi password (replace with your own)
-
-#endif
